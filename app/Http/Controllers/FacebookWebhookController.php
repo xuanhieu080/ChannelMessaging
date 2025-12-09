5ee5770e4718b6501c95f5ceb25b9961efa86131
HieuPX <xuanhieu.ht96@gmail.com>
2025-12-09 09:52:17 +0700

    Update

--- app/Http/Controllers/FacebookWebhookController.php
+++ app/Http/Controllers/FacebookWebhookController.php
@@ -9,7 +9,7 @@ class FacebookWebhookController extends Controller
 {
     public function handle(Request $request)
     {
-        Log::info("Facebook webhook received");
+        Log::info('gggggg');
         // 1) Bước VERIFY (Facebook gọi GET)
         if ($request->isMethod('get')) {
             $verifyToken = config('message-hub.facebook.verify_token');
