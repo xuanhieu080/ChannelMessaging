b2c3ae4499a67ba1d3376e13149bd4a137dc1297
HieuPX <xuanhieu.ht96@gmail.com>
2025-12-09 08:42:11 +0700

    Update

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('privacy-policy', function () {
+    return view('privacy-policy');
+});
 
 Route::match(['GET', 'POST'], '/webhook/facebook', [\App\Http\Controllers\FacebookWebhookController::class, 'handle']);
